3743d5697e288ebdd684f5c14d30930738258579
Randall Brown and Tim Hise <randall.brown+tim.hise@nordstrom.com>
2017-05-03 16:28:53 -0700

    Update formula.

--- Bluffalo.rb
+++ Bluffalo.rb
@@ -1,14 +1,17 @@
 class Bluffalo < Formula
   desc "Generate fake classes for Swift"
-  homepage ""
-
+  homepage "https://github.com/Nordstrom/bluffalo"
+  url "https://github.com/Nordstrom/bluffalo/archive/1.0.tar.gz"
   head "https://github.com/Nordstrom/bluffalo.git", :shallow => false
 
   depends_on :xcode => ["8.0", :build]
   depends_on "sourcekitten"
 
   def install
-    system "make", "prefix_install",  "PREFIX=#{prefix}", "TEMPORARY_FOLDER=#{buildpath}/Bluffalo.dst"
+    system "make", "prefix_install", "PREFIX=#{prefix}", "TEMPORARY_FOLDER=#{buildpath}/Bluffalo.dst"
   end
 
+  test do
+    system "#{bin}/bluffalo", "-?"
+  end
 end

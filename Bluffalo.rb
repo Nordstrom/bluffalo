00bb09023e4e9ae267e752f2e493f7a9133bc01e
PeqNP <eric.chamberlain@hotmail.com>
2017-07-10 08:50:25 -0700

    Point to Nordstrom repos

--- Bluffalo.rb
+++ Bluffalo.rb
@@ -1,7 +1,7 @@
 class Bluffalo < Formula
   desc "Allows you to do real mocking and stubbing in Swift."
   homepage "https://github.com/Nordstrom/bluffalo"
-  url "https://github.com/PeqNP/bluffalo/archive/1.1.tar.gz"
+  url "https://github.com/Nordstrom/bluffalo/archive/1.1.tar.gz"
   sha256 "68c4cc308382dfd73b52a67203b17e2dddc7f0b930066d198f74f861f6608730"
   head "https://github.com/Nordstrom/bluffalo.git", :shallow => false
 
